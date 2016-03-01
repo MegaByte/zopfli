e6f97e875c1625d835d957c61ee0eb9f988d1f11
Aaron Kaluszka <megabyte@kontek.net>
2016-03-01 12:32:46 -0800

    Transplant Huffman optimization from brotli

--- src/zopfli/deflate.c
+++ src/zopfli/deflate.c
@@ -384,72 +384,112 @@ static size_t CalculateBlockSymbolSize(const unsigned* ll_lengths,
   return result;
 }
 
-static size_t AbsDiff(size_t x, size_t y) {
-  if (x > y)
-    return x - y;
-  else
-    return y - x;
-}
-
 /*
 Change the population counts in a way that the consequent Huffman tree
 compression, especially its rle-part will be more likely to compress this data
 more efficiently. length containts the size of the histogram.
 */
-void OptimizeHuffmanForRle(int length, size_t* counts) {
-  int i, k, stride;
-  size_t symbol, sum, limit;
-  int* good_for_rle;
-
-  /* 1) We don't want to touch the trailing zeros. We may break the
-  rules of the format by adding more data in the distance codes. */
-  for (; length >= 0; --length) {
-    if (length == 0) {
-      return;
+unsigned OptimizeHuffmanForRle(size_t length, size_t* counts) {
+  size_t nonzero_count = 0;
+  size_t stride;
+  size_t limit;
+  size_t sum;
+  const size_t streak_limit = 1240;
+  unsigned char* good_for_rle;
+  /* 1) Let's make the Huffman code more compatible with rle encoding. */
+  size_t i;
+  for (i = 0; i < length; i++) {
+    if (counts[i]) {
+      ++nonzero_count;
     }
-    if (counts[length - 1] != 0) {
-      /* Now counts[0..length - 1] does not have trailing zeros. */
-      break;
+  }
+  if (nonzero_count < 16) {
+    return 1;
+  }
+  while (length != 0 && counts[length - 1] == 0) {
+    --length;
+  }
+  if (length == 0) {
+    return 1;  /* All zeros. */
+  }
+  /* Now counts[0..length - 1] does not have trailing zeros. */
+  {
+    size_t nonzeros = 0;
+    size_t smallest_nonzero = 1 << 30;
+    for (i = 0; i < length; ++i) {
+      if (counts[i] != 0) {
+        ++nonzeros;
+        if (smallest_nonzero > counts[i]) {
+          smallest_nonzero = counts[i];
+        }
+      }
+    }
+    if (nonzeros < 5) {
+      /* Small histogram will model it well. */
+      return 1;
+    }
+    {
+      size_t zeros = length - nonzeros;
+      if (smallest_nonzero < 4) {
+        if (zeros < 6) {
+          for (i = 1; i < length - 1; ++i) {
+            if (counts[i - 1] != 0 && counts[i] == 0 && counts[i + 1] != 0) {
+              counts[i] = 1;
+            }
+          }
+        }
+      }
+    }
+    if (nonzeros < 28) {
+      return 1;
     }
   }
   /* 2) Let's mark all population counts that already can be encoded
-  with an rle code.*/
-  good_for_rle = (int*)malloc(length * sizeof(int));
-  for (i = 0; i < length; ++i) good_for_rle[i] = 0;
-
-  /* Let's not spoil any of the existing good rle codes.
-  Mark any seq of 0's that is longer than 5 as a good_for_rle.
-  Mark any seq of non-0's that is longer than 7 as a good_for_rle.*/
-  symbol = counts[0];
-  stride = 0;
-  for (i = 0; i < length + 1; ++i) {
-    if (i == length || counts[i] != symbol) {
-      if ((symbol == 0 && stride >= 5) || (symbol != 0 && stride >= 7)) {
-        for (k = 0; k < stride; ++k) {
-          good_for_rle[i - k - 1] = 1;
+  with an rle code. */
+  good_for_rle = (unsigned char*)calloc(length, 1);
+  if (good_for_rle == NULL) {
+    return 0;
+  }
+  {
+    /* Let's not spoil any of the existing good rle codes.
+    Mark any seq of 0's that is longer as 5 as a good_for_rle.
+    Mark any seq of non-0's that is longer as 7 as a good_for_rle. */
+    size_t symbol = counts[0];
+    size_t step = 0;
+    for (i = 0; i <= length; ++i) {
+      if (i == length || counts[i] != symbol) {
+        if ((symbol == 0 && step >= 5) ||
+            (symbol != 0 && step >= 7)) {
+          size_t k;
+          for (k = 0; k < step; ++k) {
+            good_for_rle[i - k - 1] = 1;
+          }
         }
+        step = 1;
+        if (i != length) {
+          symbol = counts[i];
+        }
+      } else {
+        ++step;
       }
-      stride = 1;
-      if (i != length) {
-        symbol = counts[i];
-      }
-    } else {
-      ++stride;
     }
   }
-
-  /* 3) Let's replace those population counts that lead to more rle codes. */
+  /* 3) Let's replace those population counts that lead to more rle codes.
+  Math here is in 24.8 fixed point representation. */
   stride = 0;
-  limit = counts[0];
+  limit = 256 * (counts[0] + counts[1] + counts[2]) / 3 + 420;
   sum = 0;
-  for (i = 0; i < length + 1; ++i) {
-    if (i == length || good_for_rle[i]
-        /* Heuristic for selecting the stride ranges to collapse. */
-        || AbsDiff(counts[i], limit) >= 4) {
+  for (i = 0; i <= length; ++i) {
+    if (i == length || good_for_rle[i] ||
+        (i != 0 && good_for_rle[i - 1]) ||
+        (256 * counts[i] - limit + streak_limit) >= 2 * streak_limit) {
       if (stride >= 4 || (stride >= 3 && sum == 0)) {
+        size_t k;
         /* The stride must end, collapse what we have, if we have enough (4). */
-        int count = (sum + stride / 2) / stride;
-        if (count < 1) count = 1;
+        size_t count = (sum + stride / 2) / stride;
+        if (count == 0) {
+          count = 1;
+        }
         if (sum == 0) {
           /* Don't make an all zeros stride to be upgraded to ones. */
           count = 0;
@@ -462,13 +502,12 @@ void OptimizeHuffmanForRle(int length, size_t* counts) {
       }
       stride = 0;
       sum = 0;
-      if (i < length - 3) {
+      if (i < length - 2) {
         /* All interesting strides have a count of at least 4,
         at least when non-zeros. */
-        limit = (counts[i] + counts[i + 1] +
-                 counts[i + 2] + counts[i + 3] + 2) / 4;
+        limit = 256 * (counts[i] + counts[i + 1] + counts[i + 2]) / 3 + 420;
       } else if (i < length) {
-        limit = counts[i];
+        limit = 256 * counts[i];
       } else {
         limit = 0;
       }
@@ -476,10 +515,16 @@ void OptimizeHuffmanForRle(int length, size_t* counts) {
     ++stride;
     if (i != length) {
       sum += counts[i];
+      if (stride >= 4) {
+        limit = (256 * sum + stride / 2) / stride;
+      }
+      if (stride == 4) {
+        limit += 120;
+      }
     }
   }
-
   free(good_for_rle);
+  return 1;
 }
 
 /*
